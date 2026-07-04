94af2a759a8dc4bc3d5ea5b5423b7b61e27be257
Sumeer Khattar <sumeerkhattar@gmail.com>
2026-07-04 23:42:08 +0530

    Time: 151 ms (74.35%) | Memory: 69 MB (81.71%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -1,19 +1,20 @@
-from collections import deque
 from typing import List
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        graph = {i: [] for i in range(1, n + 1)}
+        graph = [[] for _ in range(n + 1)]
         for u, v, dist in roads:
             graph[u].append((v, dist))
             graph[v].append((u, dist))
-        visited = set([1])
-        queue = deque([1])
+        visited = [False] * (n + 1)
+        visited[1] = True
+        stack = [1]
         min_score = float('inf')
-        while queue:
-            current_city = queue.popleft()
-            for neighbor, distance in graph[current_city]:
-                min_score = min(min_score, distance)
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append(neighbor)
+        while stack:
+            node = stack.pop()
+            for neighbor, distance in graph[node]:
+                if distance < min_score:
+                    min_score = distance
+                if not visited[neighbor]:
+                    visited[neighbor] = True
+                    stack.append(neighbor)
         return min_score
